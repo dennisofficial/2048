dc734ff9705551bc4785bf13c87816458a4bdd48
Dennis Lysenko <dennislysenko@hotmail.com>
2015-08-23 14:49:57 -0700

    Update

--- src/org/croniks/j2048/room/RoomGame.java
+++ src/org/croniks/j2048/room/RoomGame.java
@@ -85,40 +85,38 @@ public class RoomGame extends JavaRoom {
 
 	@Override
 	public void draw(Graphics2D g) {
-		if (g instanceof Graphics2D) {
-			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			//Background
-			g.setColor(new Color(250, 248, 239));
-			g.fillRect(0, 0, JavaPanel.WIDTH, JavaPanel.HEIGHT);
-			//Title String
-			g.setColor(new Color(119, 110, 101));
-			g.setFont(new Font("Clear Sans", Font.BOLD, 80));
-			g.drawString("2048", 15, 80);
-			//Score Boxes
-			g.setColor(new Color(187,173,160));
-			g.fillRoundRect(390, 24, 110, 55, 6, 6);
-			g.fillRoundRect(275, 24, 110, 55, 6, 6);
-			//Score Labels
-			g.setFont(new Font("Clear Sans", Font.BOLD, 13));
-			g.setColor(new Color(238,228,218));
-			g.drawString("SCORE", 308, 45);
-			g.drawString("BEST", 425, 45);
-			//Score Numbers
-			g.setFont(new Font("Clear Sans", Font.BOLD, 25));
-			g.setColor(new Color(255, 255, 255));
-			FontMetrics fontMetrics = g.getFontMetrics(new Font("Clear Sans", Font.BOLD, 25));
-			g.drawString(score.toString(), 331 - (fontMetrics.stringWidth(score.toString())/2), 70);
-			g.drawString(best.toString(), 442 - (fontMetrics.stringWidth(best.toString())/2), 70);
-			//Grid Background
-			g.setFont(new Font("Clear Sans", Font.BOLD, 18));
-			g.setColor(new Color(187,173,160));
-			g.fillRoundRect(15, 115, 500, 500, 12, 12);
-			//Grid Tiles
-			g.setColor(new Color(238, 228, 218, 65));
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 4; j++) {
-					g.fillRoundRect(30 + (121 * i), 130 + (121 * j), 106, 106, 6, 6);
-				}
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		//Background
+		g.setColor(new Color(250, 248, 239));
+		g.fillRect(0, 0, JavaPanel.WIDTH, JavaPanel.HEIGHT);
+		//Title String
+		g.setColor(new Color(119, 110, 101));
+		g.setFont(new Font("Clear Sans", Font.BOLD, 80));
+		g.drawString("2048", 15, 80);
+		//Score Boxes
+		g.setColor(new Color(187,173,160));
+		g.fillRoundRect(390, 24, 110, 55, 6, 6);
+		g.fillRoundRect(275, 24, 110, 55, 6, 6);
+		//Score Labels
+		g.setFont(new Font("Clear Sans", Font.BOLD, 13));
+		g.setColor(new Color(238,228,218));
+		g.drawString("SCORE", 308, 45);
+		g.drawString("BEST", 425, 45);
+		//Score Numbers
+		g.setFont(new Font("Clear Sans", Font.BOLD, 25));
+		g.setColor(new Color(255, 255, 255));
+		FontMetrics fontMetrics = g.getFontMetrics(new Font("Clear Sans", Font.BOLD, 25));
+		g.drawString(score.toString(), 331 - (fontMetrics.stringWidth(score.toString())/2), 70);
+		g.drawString(best.toString(), 442 - (fontMetrics.stringWidth(best.toString())/2), 70);
+		//Grid Background
+		g.setFont(new Font("Clear Sans", Font.BOLD, 18));
+		g.setColor(new Color(187,173,160));
+		g.fillRoundRect(15, 115, 500, 500, 12, 12);
+		//Grid Tiles
+		g.setColor(new Color(238, 228, 218, 65));
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				g.fillRoundRect(30 + (121 * i), 130 + (121 * j), 106, 106, 6, 6);
 			}
 		}
 		jom.draw(g);
